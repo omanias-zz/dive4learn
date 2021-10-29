b4c42c5e6f7bae65ff012cf31cb24e8c1c9352c8
Mónica Szewczuk <mvszewczuk@gmail.com>
2021-10-29 18:03:20 -0300

    arreglos nav y home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,15 +5,17 @@ const Home = () => {
   return (
     <div>
       <img src="/img/imagen1recortadaYAchicada.png" className="home-imagen" alt="clase virtual" />
-      <div className="home-texto">
-        <h1 className="home-texto1">Optimizamos la experiencia del aprendizaje, mediante la transformación digital</h1>
-        <h3 className="home-texto2" >Somos una empresa especializada en desarrollar proyectos de educación online, 
-          mediante herramientas estratégicas para la transformación y adopción de nuevas
-          tecnologías en la educación.
-        </h3>
-      </div>
-      <div className="home-mail" >
-        <img src="/img/mail.png" className="home-mail-icono" alt="ícono mail" />
+      <div className="home">
+        <div className="home-texto">
+          <h1 className="home-texto1">Optimizamos la experiencia del aprendizaje, mediante la transformación digital</h1>
+          <div className="home-mail" >
+            <img src="/img/mail.png" className="home-mail-icono" alt="ícono mail" />
+          </div>
+          <p className="home-texto2" >Somos una empresa especializada en desarrollar proyectos de educación online, 
+            mediante herramientas estratégicas para la transformación y adopción de nuevas
+            tecnologías en la educación.
+          </p>
+        </div>
       </div>
     </div>
   );
