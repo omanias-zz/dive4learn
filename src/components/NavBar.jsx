b320da439308a77106989148437d53804da421b1
Omar Jesús Maniás <ojmanias1985@gmail.com>
2021-11-10 11:54:58 -0300

    add styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,36 +3,30 @@ import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 const NavBar = () => {
   return (
-    <div className="page-container">
+    <div>
       <Navbar collapseOnSelect expand="lg" sticky="top" className="nav-general">
-        <Container>
-          <Navbar.Brand href="/">
-            <img
-              className="nav-logo"
-              src="/img/logo.png"
-              alt="logo dive4learn"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto" id="nav-menu">
-              <Nav.Link id="nav-item" href="/">
-                HOME
-              </Nav.Link>
+        <Navbar.Brand href="/">
+          <img className="nav-logo" src="/img/logo.png" alt="logo dive4learn" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" id="nav-menu">
+            <Nav.Link id="nav-item" href="/">
+              HOME
+            </Nav.Link>
 
-              <Nav.Link id="nav-item" href="/servicios">
-                SERVICIOS
-              </Nav.Link>
+            <Nav.Link id="nav-item" href="/servicios">
+              SERVICIOS
+            </Nav.Link>
 
-              <Nav.Link id="nav-item" href="/elegirnos">
-                ¿POR QUÉ ELEGIRNOS?
-              </Nav.Link>
-              <Nav.Link id="nav-item" href="/contactanos">
-                CONTACTANOS
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+            <Nav.Link id="nav-item" href="/elegirnos">
+              ¿POR QUÉ ELEGIRNOS?
+            </Nav.Link>
+            <Nav.Link id="nav-item" href="/contactanos">
+              CONTACTANOS
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
